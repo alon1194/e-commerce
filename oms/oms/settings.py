1d2513439e68f27683c94987b28b34c5d70dc921
Alon Shrestha <a61398@alunos.ipb.pt>
2026-03-09 00:07:42 +0000

    still login

--- oms/oms/settings.py
+++ oms/oms/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'scss')
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), 
     os.path.join(BASE_DIR, 'node_modules/bootstrap/dist'),
     SASS_PROCESSOR_ROOT,
 ]
@@ -141,3 +142,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
