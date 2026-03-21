68e6d461e5f0ad34c392ddd31ebd9e6e74f69cfd
Alon Shestha <a61398@alunos.ipb.pt>
2026-03-21 21:37:46 +0000

    date of birth

--- oms/userauth/views.py
+++ oms/userauth/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView 
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class CustomLoginView(LoginView):
     template_name = "auth/login.html"
 
 class RegisterView(CreateView):
+    form_class = UserCreationForm
     template_name = "auth/Register.html"
+    
