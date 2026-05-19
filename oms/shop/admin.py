121e899f81d67bfbce1fb88498b95f82592d005f
Alon Shrestha <a61398@alunos.ipb.pt>
2026-05-19 15:29:22 +0100

    category and product problem solve

--- oms/shop/admin.py
+++ oms/shop/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import  Address
+from .models import  Address,Category, Product
 
 admin.site.register(Address)
+admin.site.register(Category)
+admin.site.register(Product)
--- oms/shop/models.py
+++ oms/shop/models.py
@@ -53,13 +53,14 @@ class Product(models.Model):
     product_slug = models.SlugField(max_length = 150, unique = True , blank = True,   null=True)
     category = models.ForeignKey(Category, on_delete = models.CASCADE, related_name = 'products')
     description = models.TextField(blank = True )
+    Image = models.ImageField( upload_to='profile_image/',null = True, blank = True)
     price = models.DecimalField(max_digits = 10 , decimal_places = 3)
     stock = models.IntegerField()
 
     def save (self, *args, **kwargs):
         self.product_slug = slugify(self.product_name)
         return super().save(*args, **kwargs)
-    def __self__(self):
+    def __str__(self):
         return self.product_name
 
 
