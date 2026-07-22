8fbe34472a0c8edbb938cd98286f2c5552d7e76b
Alon Shestha <a61398@alunos.ipb.pt>
2026-07-22 01:34:47 +0100

    responsive progress 1

--- oms/shop/admin.py
+++ oms/shop/admin.py
@@ -2,19 +2,32 @@
 
 
 from django.contrib import admin
-from .models import Product, ProductColor, ProductVariant, Category, Address
+from .models import Product, ProductColor, ProductVariant, Category, Address, Product_images
+
+
+from django.contrib import admin
+from .models import Product, ProductColor, Product_images
 
 
 class ProductColorInline(admin.TabularInline):
     model = ProductColor
-    extra = 1
+    extra = 3
+
+
+class Product_imagesInline(admin.TabularInline):
+    model = Product_images
+    extra = 3
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ProductColorInline]
-
-admin.site.register(ProductColor)
-admin.site.register(ProductVariant)
+    inlines = [
+        ProductColorInline,
+        Product_imagesInline,
+    ]
+    
 admin.site.register(Category)
 admin.site.register(Address)
+admin.site.register(ProductColor)
+admin.site.register(ProductVariant)
+admin.site.register(Product_images)
--- oms/shop/models.py
+++ oms/shop/models.py
@@ -93,3 +93,18 @@ class ProductVariant(models.Model):
     stock = models.PositiveIntegerField()
     def __str__(self):
         return f"{self.product_color.product.product_name} - {self.product_color.color} - {self.size}"
+    
+    
+    
+class Product_images(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="varaint_images"
+    )
+ 
+    variant_image = models.ImageField(upload_to="variant_products_images/")
+    def __str__(self):
+        return f"{self.product.product_name}"
+    
+    
